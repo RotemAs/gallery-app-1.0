e8098ae0dc70cd2839cce4749c17627a68dd0dad
RotemAs <91944578+RotemAs@users.noreply.github.com>
2022-06-27 16:58:16 +0300

    0.2

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, Navigate, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
 import axios from "axios";
 import apiKey from "./config";
-import PhotoContainer from './components/PhotoContainer';
-import SearchForm from './components/SearchForm';
+import PhotoContainer from "./components/PhotoContainer";
+import SearchForm from "./components/SearchForm";
 // import MainNav from './components/MainNav';
-const navKeys = ['BATTERFLAY', 'Lion', 'monkey'];
-
+const navKeys = ["BATTERFLAY", "Lion", "monkey"];
 
 function App() {
-
-  //set state for photos 
+  //set state for photos
   const [photos, setPhotos] = useState([]);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   // useEffect fetches computer photos on page load
-useEffect(() => {
-  fetchPhotos('BATTERFLAY');
+  useEffect(() => {
+    fetchPhotos("BATTERFLAY");
+  }, []);
 
-}, []);
-  
   // use axios to make call for 24 pictures
   const fetchPhotos = (query) => {
     axios
-      .get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .get(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+      )
       .then((res) => {
         console.log(res.data.photos.photo);
         setPhotos(res.data.photos.photo);
         setSearchText(query);
       })
-      .catch((err) => {console.log(err)});
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  let navKeysHtml = ''
-   for(let i = 0; i < navKeys.length; i++) 
-    navKeysHtml += `<Route path='/${navKeys[i]}' element={<PhotoContainer data={photos} searchText={'${navKeys[i]}'} /> } />`
+  let navKeysHtml = "";
+  for (let i = 0; i < navKeys.length; i++)
+    navKeysHtml += `<Route path='/${navKeys[i]}' element={<PhotoContainer data={photos} searchText={'${navKeys[i]}'} /> } />`;
   // navKeys.map((keyVal) => {
   //   `<Route path='/${keyVal}' element={<PhotoContainer data={test} searchText={'${keyVal}'} /> } />`
   // })
-   
+
   console.log(navKeysHtml);
-  
-  
+
   return (
     <BrowserRouter>
-      
       <div>
         <SearchForm />
         <nav className="main-nav">
@@ -52,29 +51,39 @@ useEffect(() => {
             {navKeys.map((key) => {
               return (
                 <li key={key}>
-                  <Link to={`/${key}`} onClick={()=> fetchPhotos(`${key}`)}>{key}</Link>
+                  <Link to={`/${key}`} onClick={() => fetchPhotos(`${key}`)}>
+                    {key}
+                  </Link>
                 </li>
               );
-            } )}
+            })}
           </ul>
         </nav>
-        
-      <Routes>
-      
-        <Route exact path='/' element={ <Navigate to='/BATTERFLAY' />} />
 
-        
-        
-        {navKeysHtml}
-        
-        <Route path='/search/:searchText' element={<PhotoContainer data={photos} searchText={searchText} /> } />
-        
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Navigate to="/BATTERFLAY" />} />
 
+          <Route
+            path="/BATTERFLAY"
+            element={<PhotoContainer data={photos} searchText={"BATTERFLAY"} />}
+          />
+          <Route
+            path="/Lion"
+            element={<PhotoContainer data={photos} searchText={"Lion"} />}
+          />
+          <Route
+            path="/monkey"
+            element={<PhotoContainer data={photos} searchText={"monkey"} />}
+          />
+
+          <Route
+            path="/search/:searchText"
+            element={<PhotoContainer data={photos} searchText={searchText} />}
+          />
+        </Routes>
       </div>
-    
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
